9888a9db287422a9fb7848d8ca3fc685e180296f
kopobaeth <philhelmuth321@gmail.com>
2026-02-20 13:12:14 +0300

    Update Base64.sol

--- contracts/libraries/Base64.sol
+++ contracts/libraries/Base64.sol
@@ -7,7 +7,7 @@
 pragma solidity ^0.8.0;
 
 /// [MIT License]
-/// @title Base646
+/// @title Base64
 /// @notice Provides a function for encoding some bytes in base64
 /// @author Brecht Devos <brecht@loopring.org>
 library Base64 {
