ead19374ed38058f3d7c47930a64024028c89d74
kopobaeth <philhelmuth321@gmail.com>
2026-02-20 13:12:51 +0300

    Update MyEpicGame.sol

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 // Helper functions OpenZeppelin provides.
-import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Counters.soll";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
 // Helper we wrote to encode in Base64
