21433894861fbf866eec6dfe0ef1294513c34af0
kopobaeth <philhelmuth321@gmail.com>
2026-02-20 13:12:01 +0300

    Rename Base64 library to Base646

--- contracts/libraries/Base64.sol
+++ contracts/libraries/Base64.sol
@@ -7,7 +7,7 @@
 pragma solidity ^0.8.0;
 
 /// [MIT License]
-/// @title Base64
+/// @title Base646
 /// @notice Provides a function for encoding some bytes in base64
 /// @author Brecht Devos <brecht@loopring.org>
 library Base64 {
